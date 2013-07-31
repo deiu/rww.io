4318a3110e0296b60d375998b9db650fae56d640
deiu <andrei@fcns.eu>
2013-07-31 23:34:46 +0200

    Utility script to check if a domain is available.

---
+++ www/root/check_domain.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once('runtime.php');
+
+$acc = urldecode($_REQUEST['domain']);
+$domain = $_ENV['CLOUD_DATA'].'/'.$acc;
+
+if (is_dir($domain))
+    httpStatusExit(200, 'OK');
+else
+    httpStatusExit(404, 'Not Found');
+exit;
